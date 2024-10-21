faba15ba0fb0111782415d58c8551dfc43b8e58d
maxrice <mxrice@loyola.edu>
2024-10-21 13:51:57 -0400

    final-design.md

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-# Code goes here and DO NOT FORGET INTRO COMMENTS
+#Programmer: Max Rice
+#Course: CS151, [Dr. Yalew]
+#Assignment due: 10/23/2024
+#Programming assignment: PA 2
+#Problem statement: program runs a game of sticks where the goal is to grab the last stick
+#Data in: Player number, amount of stick you start with, amount of sticks taken each turn, PLay again
+#Data out: Welcome, how game is played, number of sticks left, loss count for each player,thanks for playing, ask if player would like to play again
+#Purpose of program: to create a game of sticks which showcases all I have been learning in class
 import random
 
+#Set all loses equal to zero to begin with
+p1_loss = 0
+p2_loss = 0
+p3_loss = 0
+
+
 print('Welcome to the stick game')
-print('Here is how the game is played')
+print('Here is how the game is played: there will be an initial amount of sticks and the goal is to not grab the last stick \nplayer one and two will both be played by the two users and player three will be played by a computer.')
 #errorchecking goes here
 play_again = 'yes'
 while play_again == 'yes':
@@ -25,6 +38,16 @@ while play_again == 'yes':
                 player = '3'
             elif player == '3':
                 player = '1'
+#counting and displaying number of losses per player
+            if player == '1':
+                p1_loss += 1
+            if player == '2':
+                p2_loss += 1
+            if player == '3':
+                p3_loss += 1
+            print(f'player {player} lost. Thank you for playing!')
+            print('Player one losses', p1_loss, '\nPlayer two losses', p2_loss, '\nPlayer three losses', p3_loss)
+            play_again = input('Do you want to play again?(yes/no)')
 
-        print(f'player {player} lost. Thank you for playing!')
-        play_again = input('Do you want to play again?(yes/no)')
+            if play_again == 'no':
+                print('Thank you for playing!')
